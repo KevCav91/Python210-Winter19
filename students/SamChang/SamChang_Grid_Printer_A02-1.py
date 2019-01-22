09dff0b0818ba4ccc4e1baf778562238641e1316
seongtg <seongtg@yahoo.com>
2019-01-22 03:40:02 -0800

    Assignment 1

---
+++ students/SamChang/SamChang_Grid_Printer_A02-1.py
@@ -0,0 +1,23 @@
+#------------------------------------#
+#Title: Grid Printer Part 1
+#Dev: SChang
+#Date: Jan 21, 2018
+#ChangeLog: (Who, When, What)
+# SChang,01/21/2019, Created Script
+#------------------------------------#
+
+plus = '+'
+minus = '-'
+print (plus + (' ' + minus + ' ')*4 + plus,(' ' + minus + ' ')*4 + plus)
+print ("|","          ","|","           ","|")
+print ("|","          ","|","           ","|")
+print ("|","          ","|","           ","|")
+print ("|","          ","|","           ","|")
+print (plus + (' ' + minus + ' ')*4 + plus,(' ' + minus + ' ')*4 + plus)
+print ("|","          ","|","           ","|")
+print ("|","          ","|","           ","|")
+print ("|","          ","|","           ","|")
+print ("|","          ","|","           ","|")
+print (plus + (' ' + minus + ' ')*4 + plus,(' ' + minus + ' ')*4 + plus)
+
+

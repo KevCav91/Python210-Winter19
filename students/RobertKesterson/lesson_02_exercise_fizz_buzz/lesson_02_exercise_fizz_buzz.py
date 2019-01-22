ef0fd5cde39a36987b953d8b62d11fb79409a8b3
Robert Kesterson <rsk23@uw.edu>
2019-01-22 14:30:05 -0800

    Adding stub methods for different assignments

---
+++ students/RobertKesterson/lesson_02_exercise_fizz_buzz/lesson_02_exercise_fizz_buzz.py
@@ -0,0 +1,15 @@
+# -------------------------------------------- #
+# Title: Lesson 02 Exercise: Fizz Buzz
+# Desc: The classic fizz buzz interview question, spoken of in song and story
+# Change log: (who, when, what)
+# RKesterson, 2019-01-22, Created file
+# RKestesron, 2019-01-22, Stubbed out methods / parts
+# RKesterson, 2019-01-22, Completed program
+# ---------------------------------------------- #
+
+# Define the function
+def fizzBuzz():
+    print('hello')
+
+# Call the function
+fizzBuzz()
---
+++ students/RobertKesterson/lesson_02_exercise_grid_printer/lesson_02_exercise_grid_printer.py
@@ -0,0 +1,16 @@
+# -------------------------------------------- #
+# Title: Lesson 02 Exercise: Grid Printer
+# Desc: Prints out grids of different sizes using different functions
+# Change log: (who, when, what)
+# RKesterson, 2019-01-22, Created file
+# RKestesron, 2019-01-22, Stubbed out methods / parts
+# RKesterson, 2019-01-22, Completed part one
+# RKesterson, 2019-01-22, Completed part two
+# RKesterson, 2019-01-22, Completed part three
+# ---------------------------------------------- #
+
+# Part one
+
+# Part two
+
+# Part three
